dcd1319a380465952b2d0b52050618b078e42f24
mhozyaenko <mhozyaenko@gmail.com>
2019-03-26 22:59:51 +0200

    decorators hw done

--- decorator/es7functional.js
+++ decorator/es7functional.js
@@ -1,18 +1,42 @@
-// const es7 = () => {
-//
-//   class CoolMath {
-//
-//     toNumber(x) {return Number(x)}
-//     addNumbers(a.toNumber){ return a+b; }
-//     multiplyNumbers(a,b){ return a*b}
-//     minusNumbers(a,b){ return a-b }
-//   }
-//   let Calcul = new CoolMath();
-//   let x = Calcul.addNumbers(2, 2);
-//   let y = Calcul.multiplyNumbers("10", "2");
-//   let z = Calcul.minusNumbers(10, 2);
-//
-//   console.log(x, y, z);
-// };
-//
-// export default es7;
+const es7 = () => {
+
+  function number({target, key, descriptor}) {
+    const originFn = descriptor.value;
+    descriptor.value = function (...args) {
+      [...args].map(arg => Number(arg));
+      let value = originFn(...args);
+      if (isNaN(value)) {
+        console.error('can not operate arguments:', ...args)
+      } else {
+        return value
+      }
+    }
+  }
+
+  class CoolMath {
+    @number
+    addNumbers(a, b) {
+      return a + b;
+    }
+
+    @number
+    multiplyNumbers(a, b) {
+      return a * b
+    }
+
+    @number
+    minusNumbers(a, b) {
+      return a - b
+    }
+  }
+  let Calcul = new CoolMath();
+  let x = Calcul.addNumbers(2, 2)
+  let y = Calcul.multiplyNumbers("10", "2");
+  let z = Calcul.minusNumbers('ten', 2)
+  console.log('x', x)
+  console.log('y', y)
+  console.log('z', z)
+
+};
+
+export default es7;
